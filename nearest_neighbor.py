07e713f5e7ec6b7039fe27c3b394e3ff78f2441c
xherror <gxgerror@gmail.com>
2022-03-15 23:38:04 +0800

    finish knn

--- nearest_neighbor.py
+++ nearest_neighbor.py
@@ -1,3 +1,10 @@
 import imp
 import numpy as np
 from  matplotlib import pyplot as plt
+
+
+def unpickle(file):
+    import pickle
+    with open(file, 'rb') as fo:
+        dict = pickle.load(fo, encoding='bytes')
+    return dict
